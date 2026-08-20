4e6d5f4094482b7f02314588eac2e6f76a0b33b6
mccantergiani <mccantergiani@users.noreply.github.com>
2026-08-20 14:08:22 -0400

    Fuerza actualizacion inmediata del service worker (skipWaiting/clients.claim) y bloquea la orientacion en horizontal

--- sw.js
+++ sw.js
@@ -1,13 +1,15 @@
-const CACHE = 'voley-tracker-v3';
+const CACHE = 'voley-tracker-v4';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
@@ -19,8 +21,10 @@ self.addEventListener('fetch', e => {
     caches.match(e.request).then(cached => {
       if (cached) return cached;
       return fetch(e.request).then(res => {
-        const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
+        if (res.ok) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
         return res;
       }).catch(() => cached);
     })
