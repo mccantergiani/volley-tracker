58296329d3ad1899d7305fde804bc932d826ff61
mccantergiani <mccantergiani@users.noreply.github.com>
2026-08-20 12:13:07 -0400

    Soporte iOS: icono PNG para pantalla de inicio y login con redirect en vez de popup

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'voley-tracker-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const CACHE = 'voley-tracker-v2';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg', './icon-180.png', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
